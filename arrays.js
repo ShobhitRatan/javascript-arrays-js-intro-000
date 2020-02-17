1f8356802d2333c0219ffe193e553b7c6744933d
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 02:19:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [1];
+  array = ["foo",...array];
+  return array;
+}
