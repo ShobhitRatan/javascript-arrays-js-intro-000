ffacaf613f517eee64b2b1c3a52ffd2efd5d0861
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-17 02:23:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.append('foo');
+  array.pop('foo');
   return array;
 }
